7531fec4f73bc0496c6cd603c5b9d3cb4f3a7f34
Titas Roy <titas.roy@cern.ch>
2019-07-25 15:47:12 -0400

    Initial commit

---
+++ hello.c
@@ -0,0 +1,19 @@
+#include<omp.h>
+#include<stdio.h>
+
+int main()
+
+{
+
+
+#pragma omp parallel
+
+        {
+
+                printf("hello");
+                printf(" world\n");
+
+        }
+
+}
+
